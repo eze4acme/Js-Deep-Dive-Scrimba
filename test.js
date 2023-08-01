254ac72a2a44ef7b301bc68bef11ebce55a87b7d
Eze Onyebuchi <eze4acme@gmail.com>
2023-08-01 10:32:27 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 function getData (baseUrl) {
     return function (route){
-        return function (callback){
+        return function (){
             fetch(`${baseUrl}${route}`)
             .then(response => response.json())
-            .then(data => callback(data))
+            .then(data => data)
 
         }
     }
@@ -17,34 +17,34 @@ getSocialMediaPost(posts => {
 });
 
 
-function encap(){
-    let user = 'Peter Obi'
-    let message = `save Nigeria from total colapse`
-    return function () {
-    return    `${user} ${message}`
-    }
-}
-const getmsg = encap()
+// function encap(){
+//     let user = 'Peter Obi'
+//     let message = `save Nigeria from total colapse`
+//     return function () {
+//     return    `${user} ${message}`
+//     }
+// }
+// const getmsg = encap()
 
-console.log(getmsg());
-
-
-function test(plots){
-    const {main = 'Man', lay ='Faith', luty = 'Lust'} = plots
-    return `${main} is love ${lay} is bed ${luty} is bad`
-}
-
-const result = test('');
-console.log(result);
-
-let username = 'ben'
-const firstChar = name => `${name.charAt(0).toUpperCase()}${name.slice(1)}`
-console.log(firstChar(username));
-function getdetails(user, callback) {
-    return callback( firstChar(user))
-}
-
-const getResult = getdetails(username, (user) => `Hello ${user} how are you doing brother!!!`)
+// console.log(getmsg());
 
 
-console.log(getResult);
+// function test(plots){
+//     const {main = 'Man', lay ='Faith', luty = 'Lust'} = plots
+//     return `${main} is love ${lay} is bed ${luty} is bad`
+// }
+
+// const result = test('');
+// console.log(result);
+
+// let username = 'ben'
+// const firstChar = name => `${name.charAt(0).toUpperCase()}${name.slice(1)}`
+// console.log(firstChar(username));
+// function getdetails(user, callback) {
+//     return callback( firstChar(user))
+// }
+
+// const getResult = getdetails(username, (user) => `Hello ${user} how are you doing brother!!!`)
+
+
+// console.log(getResult);
