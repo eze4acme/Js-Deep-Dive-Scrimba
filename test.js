14bc79ca2fd67bf6f2a997f1bf1324f7335c9f25
Eze Onyebuchi <eze4acme@gmail.com>
2023-08-03 12:59:39 +0100

    Update test.js

--- test.js
+++ test.js
@@ -19,34 +19,34 @@ socialMediaPost(posts => posts.map(post =>{
 
 
 
-// function encap(){
-//     let user = 'Peter Obi'
-//     let message = `save Nigeria from total colapse`
-//     return function () {
-//     return    `${user} ${message}`
-//     }
-// }
-// const getmsg = encap()
+function encap(){
+    let user = 'Peter Obi'
+    let message = `save Nigeria from total colapse`
+    return function () {
+    return    `${user} ${message}`
+    }
+}
+const getmsg = encap()
 
-// console.log(getmsg());
-
-
-// function test(plots){
-//     const {main = 'Man', lay ='Faith', luty = 'Lust'} = plots
-//     return `${main} is love ${lay} is bed ${luty} is bad`
-// }
-
-// const result = test('');
-// console.log(result);
-
-// let username = 'ben'
-// const firstChar = name => `${name.charAt(0).toUpperCase()}${name.slice(1)}`
-// console.log(firstChar(username));
-// function getdetails(user, callback) {
-//     return callback( firstChar(user))
-// }
-
-// const getResult = getdetails(username, (user) => `Hello ${user} how are you doing brother!!!`)
+console.log(getmsg());
 
 
-// console.log(getResult);
+function test(plots){
+    const {main = 'Man', lay ='Faith', luty = 'Lust'} = plots
+    return `${main} is love ${lay} is bed ${luty} is bad`
+}
+
+const result = test('');
+console.log(result);
+
+let username = 'ben'
+const firstChar = name => `${name.at(0).toUpperCase()}${name.slice(1)}`
+console.log(firstChar(username));
+function getdetails(user, callback) {
+    return callback( firstChar(user))
+}
+
+const getResult = getdetails(username, (user) => `Hello ${user} how are you doing brother!!!`)
+
+
+console.log(getResult);
